620171100c822361fec4667fee4eb4306b13db7b
hokuto-kato <hokuto15@gmail.com>
2022-10-08 23:32:28 +0900

    dialog component #5

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
-import { createApp } from 'vue'
+import { createApp, defineAsyncComponent } from 'vue'
 import App from './App.vue'
 import 'destyle.css'
 const app = createApp(App)
+const BaseDialog = defineAsyncComponent(
+	() => import('./components/ui/BaseDialog.vue')
+)
+app.component('base-dialog', BaseDialog)
 app.mount('#app')
