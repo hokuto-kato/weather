137e31fc3c303734318cbc72f963a946b3a71185
hokuto-kato <hokuto15@gmail.com>
2022-10-10 19:47:54 +0900

    fix for vercel build errors #25

--- src/components/CurrentWeather.vue
+++ src/components/CurrentWeather.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script lang="ts" setup>
 import { computed } from 'vue'
 import { useCurrentWeatherStore } from '@/stores/currentWeather'
 const cw = useCurrentWeatherStore()
--- src/components/HourlyForecast.vue
+++ src/components/HourlyForecast.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script lang="ts" setup>
 import { computed } from 'vue'
 import { Chart } from 'highcharts-vue'
 import { useHourlyForecastStore } from '@/stores/hourlyForecast'
